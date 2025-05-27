9471b2bff788b9cff008bfb700cf1cef81c3c454
Michaelpraise1 <michaeld56567@gmail.com>
2025-05-26 21:00:46 -0700

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ login.addEventListener("click", function() {
   if (googleCre === emailInput.value && passwordCre === passwordInput.value) {
     alert("Login Successful")
     } else {
-    alert("Login Eror")
+    alert("Login Error")
   }
 })
 
